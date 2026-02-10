217932fa8964f4e059f6153c799c7cbddf02676e
Lokesh verma <lokeshlakshaysoni@gmail.com>
2026-02-10 12:28:29 +0530

    added Day-09

---
+++ JS-DAY-09/script09.js
@@ -0,0 +1,19 @@
+function handlechange(event) {
+    console.log(event);
+    document.getElementById("output").
+    textContent = "change detected! ";
+}
+
+
+document.getElementById("myInput").
+addEventListener("change",(event)=>{
+        // event is the synthetic event which comes under addEventListener's callback function.
+        // it will print the event object in the console which contains all the details about the form tag.
+
+    console.log(event.target.value);
+    
+    // event.target will print the element which is being targeted in the event listener, will print the user input value 
+
+    document.getElementById("output").
+    textContent = "change detected! ";
+});
